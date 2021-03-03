5c31d65f81bc24543dab53c4accd50d030ad8fec
Andrzej <intelborand@gmail.com>
2021-03-03 19:36:55 +0100

    Add JS ButtonToUp

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const btnContact = document.querySelector('.btn'),
+      expTitle = document.querySelector('.expierense__header__title'),
+      aboutMe = document.querySelector('.about-me__subtitle');
+
+
+jQuery(document).ready(function() {
+    var btn = $('#buttonUp'); 
+
+    $(window).scroll(function() {     
+          if ($(window).scrollTop() > 300) {
+             btn.addClass('show');
+           } else {
+             btn.removeClass('show');
+           }
+    });
+    
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+});
